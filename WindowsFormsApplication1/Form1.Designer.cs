83b7d93be29d79f59b2a66aeec89b57a94fd4e23
Sergey Barkov <sergeybarkov@outlook.com>
2016-03-17 11:26:15 +0300

    1st commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.taskList = new System.Windows.Forms.CheckedListBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сделать как надо";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 195);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(327, 35);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(121, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Готово $1 из $2";
+            this.label1.Visible = false;
+            // 
+            // taskList
+            // 
+            this.taskList.CheckOnClick = true;
+            this.taskList.FormattingEnabled = true;
+            this.taskList.Items.AddRange(new object[] {
+            "Очистить целевую папку",
+            "Преобразовать документы в PDF",
+            "Скопировать в целевую папку непреобразованное",
+            "Сгенерировать HTML-меню"});
+            this.taskList.Location = new System.Drawing.Point(12, 110);
+            this.taskList.Name = "taskList";
+            this.taskList.Size = new System.Drawing.Size(327, 79);
+            this.taskList.TabIndex = 5;
+            this.taskList.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(12, 23);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ReadOnly = true;
+            this.txtSource.Size = new System.Drawing.Size(295, 20);
+            this.txtSource.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(313, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Исходная папка:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Целевая папка:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(313, 64);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(26, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(12, 65);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.ReadOnly = true;
+            this.txtDest.Size = new System.Drawing.Size(295, 20);
+            this.txtDest.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Задачи:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.taskList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(370, 300);
+            this.MinimumSize = new System.Drawing.Size(370, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "All To PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox taskList;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+//using w = Microsoft.Office.Interop.Word;
+//using p = Microsoft.Office.Interop.PowerPoint;
+
+using Aspose.Words;
+
+using System.IO;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //protected Microsoft.Office.Interop.Word.Application word;
+        //protected Microsoft.Office.Interop.PowerPoint.Application powerpoint;
+        //protected object oMissing = System.Reflection.Missing.Value;
+
+        String inDir = @"D:\_ДО";
+        String outDir = @"D:\_ДО_TEST";
+        String htmlTree;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (var i = 0; i < taskList.Items.Count; i++ ) taskList.SetItemChecked(i, true);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //STEP 0: Проверка
+            if (String.IsNullOrWhiteSpace(txtSource.Text))
+            {
+                MessageBox.Show("Не указана исходная папка!");
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtDest.Text))
+            {
+                MessageBox.Show("Не указана целевая папка!");
+                return;
+            }
+
+            inDir = txtSource.Text;
+            outDir = txtDest.Text;
+
+            //STEP 1: Очистка целевой папки
+            if (taskList.GetItemChecked(0))
+            {
+                emptyDir(outDir);
+            }
+
+            //STEP 2: Преобразование в PDF
+            if (taskList.GetItemChecked(1))
+            {
+
+
+                button1.Visible = false;
+                button2.Visible = false;
+                progressBar1.Visible = true;
+
+                //word = new Microsoft.Office.Interop.Word.Application();
+                //powerpoint = new Microsoft.Office.Interop.PowerPoint.Application();
+
+                DirectoryInfo dirInfo = new DirectoryInfo(inDir);
+                FileInfo[] wordFiles = dirInfo.GetFiles("*.*", SearchOption.AllDirectories);
+
+                progressBar1.Maximum = wordFiles.Count();
+
+                //word.Visible = false;
+                //word.ScreenUpdating = false;
+                //            powerpoint.Visible = Microsoft.Office.Core.MsoTriState.msoFalse;
+
+
+                var count = wordFiles.Count();
+
+                var i = 0;
+
+                label1.Text = "";
+                label1.Visible = true;
+
+
+                foreach (FileInfo wordFile in wordFiles)
+                {
+                    try
+                    {
+                        try
+                        {
+
+                            Directory.CreateDirectory(wordFile.DirectoryName.Replace(inDir, outDir));
+                        }
+                        catch (Exception ex) { ; }
+
+                        if (wordFile.Name.StartsWith("~"))
+                        {
+                            continue;
+                        }
+
+                        if (wordFile.Extension.ToLower() == ".doc" || wordFile.Extension.ToLower() == ".docx" || wordFile.Extension.ToLower() == ".rtf")
+                        {
+                            DocToPdf(wordFile);
+                        }
+                        /*else if (wordFile.Extension.ToLower() == ".ppt" || wordFile.Extension.ToLower() == ".pptx" || wordFile.Extension.ToLower() == ".pps" || wordFile.Extension.ToLower() == ".ppsx")
+                        {
+                            PptToPdf(wordFile);
+                        }*/
+                        else if (wordFile.Extension.ToLower() != ".db")
+                        {
+                            //Подшаг 2.1 Копируем непреобразованный хлам
+                            if (taskList.GetItemChecked(2))
+                            {
+                                File.Copy(wordFile.FullName, wordFile.FullName.Replace(inDir, outDir));
+                            }
+                        }
+
+                        progressBar1.Value = ++i;
+                        label1.Text = String.Format("Готово {0} из {1}", i.ToString(), count.ToString());
+                        System.Windows.Forms.Application.DoEvents();
+
+
+
+                    }
+                    catch (Exception error)
+                    {
+                        ;
+                    }
+
+
+                }
+
+                //((w._Application)word).Quit(ref oMissing, ref oMissing, ref oMissing);
+                //word = null;
+                //powerpoint.Quit();
+                //powerpoint = null;
+
+            }
+
+            if (taskList.GetItemChecked(3))
+            {
+                CreateMenu(outDir);
+
+                var dirs = Directory.GetDirectories(outDir);
+                foreach (var dir in dirs)
+                {
+                    CreateMenu(dir);
+                }
+            }
+
+            button1.Visible = true;
+            button2.Visible = true;
+            progressBar1.Visible = false;
+        }
+
+        protected void DocToPdf(FileInfo wordFile)
+        {
+            var document = new Document(wordFile.FullName);
+
+            document.Save(wordFile.FullName.Replace(wordFile.Extension, ".pdf").Replace(inDir, outDir), SaveFormat.Pdf);
+
+            /*Object filename = (Object)wordFile.FullName;
+
+            w.Document doc = word.Documents.Open(ref filename, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+            doc.Activate();
+
+            object outputFileName = wordFile.FullName.Replace(wordFile.Extension, ".pdf").Replace(inDir, outDir);
+            object fileFormat = w.WdSaveFormat.wdFormatPDF;
+
+            // Save document into PDF Format
+            doc.SaveAs(ref outputFileName,
+                ref fileFormat, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+            object saveChanges = w.WdSaveOptions.wdDoNotSaveChanges;
+            ((w._Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+            doc = null;*/
+        }
+
+        protected void PptToPdf(FileInfo pptFile)
+        {
+            /*String filename = pptFile.FullName;
+            p.Presentation ppt = powerpoint.Presentations.Open(filename);
+            powerpoint.Activate();
+
+            String outputFileName = pptFile.FullName.Replace(pptFile.Extension, ".pdf").Replace(".pptx", ".pdf").Replace(inDir, outDir);
+            ppt.SaveAs(outputFileName, p.PpSaveAsFileType.ppSaveAsPDF);
+
+            ppt.Close();*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void CreateMenu(string rootFolder)
+        {
+            if (rootFolder.Contains(@"\menu")) return;
+
+            htmlTree = "";
+
+            outDir = rootFolder;
+
+            var dirs = Directory.GetDirectories(outDir);
+            foreach (var dir in dirs)
+            {
+                if(dir.Replace(outDir, "") != @"\menu")
+                    WalkDirectoryTree(new DirectoryInfo(dir));
+            }
+            var files = Directory.GetFiles(outDir);
+
+            var currentDir = new DirectoryInfo(outDir);
+
+            if (currentDir.GetFiles().Count() > 0)
+            {
+                htmlTree += "<ul>";
+                foreach (System.IO.FileInfo fi in currentDir.GetFiles())
+                {
+                    htmlTree += "<li><span class='file'><a href='" + fi.FullName.Replace(outDir + @"\", @"../").Replace(@"\", @"/") + "' target='mainFrame'>" + fi.Name.Replace(fi.Extension, "") + "</a></span></li>";
+                }
+                htmlTree += "</ul>";
+            }
+
+            CopyFolder(@"menu_template", outDir + @"\menu");
+
+            var template = File.ReadAllText(@"menu_template\menu.html");
+
+            template = template.Replace("{tree}", htmlTree.Substring(4).Replace(@"</ul><ul>", @"</ul><ul class='filetree treeview-famfamfam'>"));
+            template = template.Replace("{title}", currentDir.Name);
+           
+
+            File.WriteAllText(outDir + @"\menu\menu.html", template);
+        }
+
+        protected void CopyFolder(string source, string target)
+        {
+            if (!Directory.Exists(target))
+                Directory.CreateDirectory(target);
+
+            string[] files = Directory.GetFiles(source);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(target, name);
+                File.Copy(file, dest, true);
+            }
+            string[] folders = Directory.GetDirectories(source);
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(target, name);
+                CopyFolder(folder, dest);
+            }
+        }
+
+        //Спасибо примерам из MSDN за то, что не заставляют нас думать =)
+        private void WalkDirectoryTree(System.IO.DirectoryInfo root)
+        {
+
+            System.IO.FileInfo[] files = null;
+            System.IO.DirectoryInfo[] subDirs = null;
+
+            htmlTree += "<ul><li><span class='folder'>" + root.Name + "</span>";
+
+            // First, process all the files directly under this folder
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+            catch (Exception e)
+            {
+                ;
+                
+            }
+
+            if (files != null)
+            {
+
+                htmlTree += "<ul>";
+
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    htmlTree += "<li><span class='file'><a href='" + fi.FullName.Replace(outDir +@"\", @"../").Replace(@"\", @"/") + "' target='mainFrame'>" + fi.Name.Replace(fi.Extension, "") + "</a></span></li>";
+                }
+
+                htmlTree += "</ul>";
+
+                // Now find all the subdirectories under this directory.
+                subDirs = root.GetDirectories();
+
+                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
+                {
+                    if (dirInfo.Name == "menu") continue; 
+                    WalkDirectoryTree(dirInfo);
+                }
+
+                
+            }
+            htmlTree += "</li></ul>";
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSource.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtDest.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        protected void emptyDir(string dirPath)
+        {
+            System.IO.DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+        }
+    }
+}
